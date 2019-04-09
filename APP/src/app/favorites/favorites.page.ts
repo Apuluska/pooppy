02547c3a0899da05922159cc935eb3a0fe028878
BarbyMarquina <bsmp1993@gmail.com>
2019-04-09 09:36:13 +0200

    merge branch celia

--- APP/src/app/favorites/favorites.page.ts
+++ APP/src/app/favorites/favorites.page.ts
@@ -13,37 +13,34 @@ export class FavoritesPage implements OnInit {
     private usersService: UsersService,
     ) {}
 
-  ngOnInit() {
-    this.getFavoriteBinData()
-  }
-
-  public bins = [];
- 
-//userId es el id del usuario logueado, hay que ver de donde lo saca el front
-  //getFavoriteBinData(userId: string): any { //Esta es la linea que deberia quedar, debe recibir el userid por parametro
-  //this.bins = [];
-  getFavoriteBinData(): any {
-    console.log("vamoalyeu");
-    let userId = "5c9b28545f02671f443fb996";
-    this.usersService.getUserFavoriteBinsData(userId)
-      .subscribe(
-      (bin_observable) => {
-       // bin_observable.length
-       this.bins = [];
-        for(let i = 0; i < bin_observable.length;i++){
-          console.log("a pushear " + bin_observable[i].address[0].addressName);
-          this.bins.push(bin_observable[i]);
-         
-        } 
-    
-      });
-  }
-  public deleteBin(binId):void {
-    let userId = "5c9b28545f02671f443fb996";
-    console.log(binId)
-    this.usersService.deleteBin(userId,binId).
-    subscribe(
+    ngOnInit() {
       this.getFavoriteBinData()
-    );
+    }
+  
+    public bins = [];
+   
+  //userId es el id del usuario logueado, hay que ver de donde lo saca el front
+    //getFavoriteBinData(userId: string): any { //Esta es la linea que deberia quedar, debe recibir el userid por parametro
+    //this.bins = [];
+    getFavoriteBinData(): any {
+      console.log("vamoalyeu");
+      let userId = "5c9b28545f02671f443fb996";
+      this.usersService.getUserFavoriteBinsData(userId)
+        .subscribe(
+        (bin_observable) => {
+         this.bins = [];
+          for(let i = 0; i < bin_observable.length;i++){
+            this.bins.push(bin_observable[i]);
+          } 
+        });
+    }
+  
+    public deleteBin(binId):void {
+      let userId = "5c9b28545f02671f443fb996";
+      console.log("estoy eliminando")
+      this.usersService.deleteBin(userId,binId).
+      subscribe(
+        this.getFavoriteBinData()
+      );
+    }
   }
-}
--- APP/src/app/services/users.service.ts
+++ APP/src/app/services/users.service.ts
@@ -29,8 +29,11 @@ export class UsersService {
     return this.http.get<Bin[]>(endpointUrl);
   }
 
-  deleteBin( userId: string, idBin:string) : Observable<string> {
-    return this.http.put<Bin["_id"]>(`${this.userUrl}/favoritebins/update`, {userId, idBin}, this.httpOptions)
+  deleteBin( id_user: string, id_bin:string) : Observable<string> {
+    console.log("estoy en el servicio");
+    console.log(id_user);
+    console.log(id_bin);
+    return this.http.put<Bin["_id"]>(`${this.userUrl}/favoritebins/update`, {id_user, id_bin}, this.httpOptions)
+   
   }
-
 }
